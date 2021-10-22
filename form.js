079a9eaa647ee76885031f384a2e62fd18cc32fa
informcity <moroco111@i.ua>
2021-10-22 16:13:08 +0300

    3

--- form.js
+++ form.js
@@ -12,11 +12,15 @@ document.body.addEventListener('click', (e) => {
   }
 });
 
-form.addEventListener('submit', (e) => {
+/*form.addEventListener('submit', (e) => {
   e.preventDefault();
   console.log(form.name.value);
   wrapper.className = "wrapper";
   wrapFlex.innerHTML = `<h1>new Goods("${form.number.value}", "${form.pastNum.value}", "${form.name.value}", "${form.price.value}", "${form.price75.value}", "${form.price50.value}")</h1>`;
   form.reset(); 
-});
+});*/
+
+let sum = [ 240, 200, 170, 200, 310, 250, 290, 190, 320, 210, 75, 145, 180, 125, 160, 160, 60, 175, 80];
+console.log((arrGoods.reduce((acum, item) => acum + +item.price, 0)) + (sum.reduce((s,i)=> s+i, 0)));
+
 
